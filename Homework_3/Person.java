1e76978394022c6efa349ef576550466e526dbdc
Shtora Pavel <73071735+jRPGFan@users.noreply.github.com>
2020-12-07 15:27:34 +0200

    Homework_3

---
+++ Homework_3/Person.java
@@ -0,0 +1,15 @@
+package Homework_3;
+
+import java.util.ArrayList;
+
+public class Person {
+    //protected String personName;
+    protected String personPhone;
+    protected String personEmail;
+
+    Person(String personPhone, String personEmail){
+        //this.personName = personName;
+        this.personPhone = personPhone;
+        this.personEmail = personEmail;
+    }
+}
